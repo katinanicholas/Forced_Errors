3cbf9aba2837604a360975b9730a5f0eb0a11306
katinanicholas <bodden_k@yahoo.com>
2021-07-25 23:11:26 -0500

    First version

---
+++ src/com/company/Main.java
@@ -0,0 +1,33 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // Illegal Field Names
+//        String year = 2019;
+//        boolean runtime = 3.54;
+//        int genre = "R";
+//        char isOpposite = true;
+//        double title = "JAWS";
+//        System.out.println();
+//    }
+
+        // Unintialized Functiom
+//        int x;
+//        if (condition) {
+//            x = 3;
+//        }
+    }
+//        System.out.println(x); // x may not have been initialized
+
+    // Unintialized Functiom
+    {
+        int a = 2;
+        int b;
+        int c = a + b;
+    }
+}
+
+
+
+
